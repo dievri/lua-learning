97e5f1f92eee359b62e85038c1cb9e1c12826fde
Oleksandr Dievri <o.dievri@gmail.com>
2020-11-06 00:04:50 +0200

    add closure function generator example

---
+++ 005_less_than.lua
@@ -0,0 +1,18 @@
+#!/usr/bin/lua
+-- Returns a fucntion that tests whether a number is less than N:
+function MakeLessThan(N)
+    return function(X)
+        return X < N
+    end
+end
+
+LessThanFive = MakeLessThan(5)
+LessThanTen = MakeLessThan(10)
+print(LessThanFive(4))
+print(LessThanTen(4))
+print(LessThanFive(5))
+print(LessThanTen(5))
+print(LessThanFive(9))
+print(LessThanTen(9))
+print(LessThanFive(10))
+print(LessThanTen(10))
